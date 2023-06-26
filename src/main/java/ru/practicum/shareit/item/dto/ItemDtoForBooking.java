3359a4ba38f32cbf15d21cc0bf4c3426eaaf877d
Kaminari <Opk26rus@yandex.ru>
2023-06-26 21:39:50 +0300

    add requests and tests

--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoForBooking.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoForBooking.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.comment.dto.CommentDto;
 
